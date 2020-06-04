600cfef4a8a3c5105b08205bd5857e76ecb8a112
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-06-04 16:03:23 +0200

    Finish 9.3 part 1

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface argsAsNumbers {
+  height: number,
+  weight: number
+}
+
+const parseArguments = (args: Array<string>): argsAsNumbers => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const calculateBmi = (height: number, weight: number) => {
   const heightInMetres = height/100
   const result = weight / (heightInMetres * heightInMetres)
@@ -22,4 +41,9 @@ const calculateBmi = (height: number, weight: number) => {
   }
 }
 
-console.log(calculateBmi(180, 70))
+try {
+  const { height, weight } = parseArguments(process.argv)
+  console.log(calculateBmi(height, weight))
+} catch (e) {
+  console.log('Error, ', e.message)
+}
